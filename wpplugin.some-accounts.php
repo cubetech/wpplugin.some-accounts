ae288212d85af6f10f44bb6131e91e9420f221d2
culas <lucas.schnueriger@me.com>
2015-05-18 09:43:40 +0200

    first commit

---
+++ wpplugin.some-accounts.php
@@ -0,0 +1,122 @@
+<?php
+/*
+Plugin Name: Cubetech Wordpress SoMe Accounts
+Plugin URI: http://cubetech.ch
+Description: A plugin to add your social media accounts
+Version: 1.0
+Author: Lucas Schnüriger
+Author URI: http://cubetech.ch
+*/
+
+if ( !defined( 'RWMB_VER' ) ) {
+	include('metabox.io/meta-box.php');
+}
+
+class Wordpress_some_accounts{
+	function init(){
+		add_action('init', array($this, 'someRegisterAction'));
+// 		add_action('admin_init', array($this, 'addIconPreviewFieldAction'));
+		add_filter('rwmb_meta_boxes', array($this, 'registerMetaboxesAction'));
+	}
+	
+	function someRegisterAction() {
+		$labels = array(
+			'name'               => _x( 'Social Media Accounts', 'someaccs', 'wpplugin-someaccs' ),
+			'singular_name'      => _x( 'SoMe Account', 'someacc', 'wpplugin-someaccs' ),
+			'menu_name'          => _x( 'SoMe Accounts', 'someaccs', 'wpplugin-someaccs' ),
+			'name_admin_bar'     => _x( 'SoMe Account', 'someaccs', 'wpplugin-someaccs' ),
+			'add_new'            => _x( 'Neuer Account', 'someacc', 'wpplugin-someaccs' ),
+			'add_new_item'       => __( 'Neuer Account', 'wpplugin-someaccs' ),
+			'new_item'           => __( 'Neuer Account', 'wpplugin-someaccs' ),
+			'edit_item'          => __( 'Account bearbeiten', 'wpplugin-someaccs' ),
+			'view_item'          => __( 'Account anschauen', 'wpplugin-someaccs' ),
+			'all_items'          => __( 'Alle Accounts', 'wpplugin-someaccs' ),
+			'search_items'       => __( 'Accounts durchsuchen', 'wpplugin-someaccs' ),
+			'parent_item_colon'  => __( 'Parent Accounts:', 'wpplugin-someaccs' ),
+			'not_found'          => __( 'Keine Accounts gefunden', 'wpplugin-someaccs' ),
+			'not_found_in_trash' => __( 'Keine Accounts im Papierkorb', 'wpplugin-someaccs' )
+		);
+
+		$args = array(
+			'labels'             => $labels,
+			'menu_icon'			 => 'dashicons-share',
+			'public'             => true,
+			'publicly_queryable' => true,
+			'show_ui'            => true,
+			'show_in_menu'       => true,
+			'query_var'          => true,
+			'rewrite'            => array( 'slug' => 'someaccs' ),
+			'capability_type'    => 'post',
+			'has_archive'        => false,
+			'hierarchical'       => false,
+			'menu_position'      => null,
+			'supports'           => array( 'title' )
+		);
+
+		register_post_type('someaccs', $args);
+	}
+	
+	function registerMetaboxesAction( $meta_boxes )
+	{
+	    $prefix = 'ct_some_';
+
+	    // 1st meta box
+	    $meta_boxes[] = array(
+	        'id'       => 'account_information',
+	        'title'    => 'Account Informationen',
+	        'pages'    => array( 'someaccs' ),
+	        'context'  => 'normal',
+	        'priority' => 'high',
+	
+	        'fields' => array(
+	            array(
+	                'name'  => 'Link',
+	                'desc'  => 'Link zum Account auf der Plattform',
+	                'id'    => $prefix . 'link',
+	                'std'	=> 'http://',
+	                'type'  => 'url',
+	            ),
+	            array(
+	                'name'  => 'Icon',
+	                'desc'  => 'verwendet FontAwesome Icons (http://fontawesome.io/icons/#brand)',
+	                'id'    => $prefix . 'icon',
+	                'type'  => 'select',
+	                'options' => array(
+		                'facebook' => 'Facebook',
+		                'facebook-square' => 'Facebook (quadratisch)',
+		                'facebook-official' => 'Facebook (offiziell)',
+		                'twitter' => 'Twitter',
+		                'twitter-square' => 'Twitter (quadratisch)',
+		                'instagram' => 'Instagram',
+		                'xing' => 'XING',
+		                'xing-square' => 'XING (quadratisch)',
+		                'linkedin' => 'LinkedIn',
+		                'linkedin-square' => 'LinkedIn (quadratisch)',
+		                'pinterest' => 'Pinterest',
+		                'pinterest-square' => 'Pinterest (quadratisch)',
+		                'tumblr' => 'Tumblr',
+		                'tumblr-square' => 'Tumblr (quadratisch)',
+		                'youtube' => 'YouTube',
+		                'youtube-square' => 'YouTube (quadratisch)',
+		                'youtube-play' => 'YouTube (Play Icon)',
+		                'dribbble' => 'Dribbble',
+		                'deviantart' => 'DeviantArt',
+		                'behance' => 'Behance',
+		                'behance-square' => 'Behance (quadratisch)',
+		                'github' => 'GitHub',
+		                'github-square' => 'GitHub (quadratisch)',
+		                'wordpress' => 'Wordpress',
+		                'share-alt' => 'Share neutral',
+		                'share-alt-square' => 'Share neutral (quadratisch)',
+	                )
+	            ),
+	        )
+	    );
+	
+	    return $meta_boxes;
+	}
+	
+}//end class
+
+$var = new Wordpress_some_accounts();
+$var->init();
